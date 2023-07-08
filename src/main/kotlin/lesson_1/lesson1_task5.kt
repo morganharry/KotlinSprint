c1ab5b53841c4874523dfcd1822c90b4f9600ab1
HM <west_ham@mail.ru>
2023-07-08 20:50:11 +0600

    KS-1-5 add time of flight in format min:sec

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,5 +1,9 @@
 package lesson_1
 
 fun main() {
-
+    val sumOfSeconds = 6480
+    val secInMin = 60
+    val numOfMinuts = sumOfSeconds / secInMin
+    val numOfSeconds = sumOfSeconds % secInMin
+    println("$numOfMinuts:${String.format("%02d", numOfSeconds)}")
 }
