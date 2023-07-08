e9ebae467651ac25aaac732fb3c847a484a0970e
HM <west_ham@mail.ru>
2023-07-08 22:10:29 +0600

    KS-1-5 fixed typo in variable name

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -3,7 +3,7 @@ package lesson_1
 fun main() {
     val sumOfSeconds = 6480
     val secInMin = 60
-    val numOfMinuts = sumOfSeconds / secInMin
+    val numOfMinutes = sumOfSeconds / secInMin
     val numOfSeconds = sumOfSeconds % secInMin
-    println("$numOfMinuts:${String.format("%02d", numOfSeconds)}")
+    println("$numOfMinutes:${String.format("%02d", numOfSeconds)}")
 }
