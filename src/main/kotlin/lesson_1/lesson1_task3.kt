cf52f12595a2c6783d4bfd71373f2888f3eac080
HM <west_ham@mail.ru>
2023-07-08 20:01:57 +0600

    KS-1-2 add the numbers of employees Added variables with the numbers of employees, changed and printed the numbers.

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,12 @@
 package lesson_1
 
 fun main() {
-
+    val numOfOrders = 75
+    val thxForOrder = "Thank you for your order."
+    println(numOfOrders)
+    println(thxForOrder)
+    var numOfEmployees = 2000
+    //println(numOfEmployees)
+    numOfEmployees--
+    println(numOfEmployees)
 }
