ae9638e2c392d52f069bbeff7b9e57e6c685014c
HM <west_ham@mail.ru>
2023-07-08 22:07:54 +0600

    KS-1-4 added missing data types

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -5,8 +5,8 @@ fun main() {
     val age: Byte = 27
     val partOfDay: Float = 0.075f
     val seconds: Short = 6480
-    val partOfYear = 2.0547945205479453E-4
-    val apogee = 327000
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
     println(length)
     println(age)
     println(partOfDay)
