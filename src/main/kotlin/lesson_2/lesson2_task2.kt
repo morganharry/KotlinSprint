6eb90694aaa397905ce8d912cf2d7be9dfce4f61
HM <west_ham@mail.ru>
2023-07-09 15:17:37 +0600

    KS-2-2 added values and calculated the required data

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,4 +1,16 @@
 package lesson_2
 
 fun main() {
+    val numOfEmployees = 50
+    val salaryOfEmployees = 30_000
+    val numOfInterns = 30
+    val salaryOfInterns = 20_000
+
+    val sumSalaryOfEmployees = numOfEmployees * salaryOfEmployees
+    val sumSalaryOfAll = sumSalaryOfEmployees + numOfInterns * salaryOfInterns
+    val averageSalaryOfAll = sumSalaryOfAll / (numOfEmployees + numOfInterns)
+
+    println (sumSalaryOfEmployees)
+    println (sumSalaryOfAll)
+    println (averageSalaryOfAll)
 }
