648c5b1f4f596b0a889790a552c117f77e3ace06
HM <west_ham@mail.ru>
2023-07-09 15:59:26 +0600

    KS-2-5 added values and calculated the required data

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,4 +1,12 @@
 package lesson_2
 
+import kotlin.math.*
+
 fun main() {
+    var deposit = 70_000.00
+    val rateDeposit = 16.7 / 100
+    val termDeposit = 20
+
+    deposit = deposit * (1.0 + rateDeposit).pow(termDeposit)
+    println((deposit * 1000.0).roundToInt() / 1000.0)
 }
