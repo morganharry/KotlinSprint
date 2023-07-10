f17435092a4a5bafb6dda27b8ee375cc4db1410d
HM <west_ham@mail.ru>
2023-07-10 14:16:37 +0600

    KS-3-5 added variables and output

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,5 +1,10 @@
 package lesson_3
 
 fun main() {
+    val move = "D2-D4;22"
+    val startMove = move.substring(0, 2)
+    val endMove = move.substring(3, 5)
+    val numMove = move.substring(6)
 
+    println("$startMove-$endMove;$numMove")
 }
