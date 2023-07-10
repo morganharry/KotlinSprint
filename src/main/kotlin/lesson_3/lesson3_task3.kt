ff939573fed44f304e300a1c788c39dbc266de34
HM <west_ham@mail.ru>
2023-07-10 14:01:11 +0600

    KS-3-3 added variables and output

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -1,5 +1,18 @@
 package lesson_3
 
 fun main() {
-
+    val num = 6
+    println(
+        """
+        $num x 1 = ${num * 1}
+        $num x 2 = ${num * 2}
+        $num x 3 = ${num * 3}
+        $num x 4 = ${num * 4}
+        $num x 5 = ${num * 5}
+        $num x 6 = ${num * 6}
+        $num x 7 = ${num * 7}
+        $num x 8 = ${num * 8}
+        $num x 9 = ${num * 9}        
+    """.trimIndent()
+    )
 }
