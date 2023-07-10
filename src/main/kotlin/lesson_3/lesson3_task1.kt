0ecb81154aa0f2fd44b8179e0cc6b5769baede47
HM <west_ham@mail.ru>
2023-07-10 13:40:17 +0600

    KS-3-1 added variables and output

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -1,10 +1,10 @@
 package lesson_3
 
 fun main() {
-    var hello = "Добрый день, "
-    val name = "Николай"
+    var hello = "Добрый день,"
+    val name = " Николай"
 
     println(hello + name)
-    hello = "Добрый вечер, "
+    hello = "Добрый вечер,"
     println(hello + name)
 }
