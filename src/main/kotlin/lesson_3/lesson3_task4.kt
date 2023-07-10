f172a3bf5df382f32d104ffaaf4088d22d374c2a
HM <west_ham@mail.ru>
2023-07-10 14:08:02 +0600

    KS-3-4 added variables and output

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,5 +1,13 @@
 package lesson_3
 
 fun main() {
+    var startMove = "E2"
+    var endMove = "E4"
+    var numMove = 1
 
+    println("$startMove-$endMove, $numMove")
+    startMove = "D2"
+    endMove = "D3"
+    numMove++
+    println("$startMove-$endMove, $numMove")
 }
