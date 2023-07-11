b49ba5f3ac2f4c5857856abd0399f7ed7c20beac
HM <west_ham@mail.ru>
2023-07-11 14:23:42 +0600

    KS-4-2 added and changed variables, compare in output

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,5 +1,22 @@
 package lesson_4
 
 fun main() {
+    val minWeightAverage = 35
+    val maxWeightAverage = 100
+    val maxVolumeAverage = 100
 
+    var weightСargo = 20
+    var volumeСargo = 80
+    println(
+        "Груз с весом ${weightСargo} кг и объемом ${volumeСargo} л соответствует категории 'Average': " +
+                "${(weightСargo > minWeightAverage && weightСargo <= maxWeightAverage && volumeСargo < maxVolumeAverage)}"
+    )
+
+    weightСargo = 50
+    volumeСargo = 100
+    println(
+        "Груз с весом ${weightСargo} кг и объемом ${volumeСargo} л соответствует категории 'Average': " +
+                "${(weightСargo > minWeightAverage && weightСargo <= maxWeightAverage && volumeСargo < maxVolumeAverage)}"
+    )
 }
+
