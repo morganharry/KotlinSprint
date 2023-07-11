0010f76df688c37682bfeeca3ae416ce7a594cb9
HM <west_ham@mail.ru>
2023-07-11 21:34:07 +0600

    KS-4-5 changed names of variables, added const's

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,29 +1,27 @@
 package lesson_4
 
+const val NO_DAMAGE = true
+const val MIN_TEAM = 55
+const val MAX_TEAM = 70
+const val MIN_FOOD = 50
+const val GOOD_WEATHER = true
+
 fun main() {
-    println("Корабль не имеет повреждений? (true/false)")
-    val damage = readln().toBoolean()
+    println("Корабль поврежден? (true/false)")
+    val isDamage = readln().toBoolean()
     println("Текущий состав экипажа? (количество)")
     val numOfTeam = readln().toInt()
     println("Количество ящиков с провизией на борту? (количество)")
-    val numOfFoods = readln().toInt()
+    val numOfFood = readln().toInt()
     println("Благоприятность метеоусловий? (true/false)")
-    val goodWeather = readln().toBoolean()
+    val isGoodWeather = readln().toBoolean()
 
-    val requiredDamageLongTrip = true
-    val minNumOfTeamLongTrip = 55
-    val maxNumOfTeamLongTrip = 70
-    val minFoodsLongTrip = 50
-
-    val NumOfTeamTrip = 70
-    val minFoodsTrip = 50
-    val requiredGoodWeatherTrip = true
 
     when {
-        (damage == requiredDamageLongTrip && numOfTeam in (minNumOfTeamLongTrip..maxNumOfTeamLongTrip) && numOfFoods > minFoodsLongTrip) ->
+        (isDamage != NO_DAMAGE && numOfTeam in (MIN_TEAM..MAX_TEAM) && numOfFood > MIN_FOOD) ->
             println("Научно-исследовательский корабль может приступить к долгосрочному плаванию.")
 
-        (numOfTeam == NumOfTeamTrip && numOfFoods > minFoodsTrip && goodWeather == requiredGoodWeatherTrip) ->
+        (numOfTeam == MAX_TEAM && numOfFood > MIN_FOOD && isGoodWeather == GOOD_WEATHER) ->
             println("Научно-исследовательский корабль может отправиться в плавание.")
 
         else -> println("Научно-исследовательский корабль не может отправиться в плавание.")
