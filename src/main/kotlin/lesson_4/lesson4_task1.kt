a9b0e3f090c3585f857dda9a80a97c0384cde6cf
HM <west_ham@mail.ru>
2023-07-11 13:53:33 +0600

    KS-4-1 added variables and compare in output Why in the conditions the entity of "tables of everything"?

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,5 +1,8 @@
 package lesson_4
 
 fun main() {
+    val todayFreeTable = 0
+    val tomorrowFreeTable = 4
 
+    println("Доступность столиков на сегодня: ${(todayFreeTable) > 0},\nДоступность столиков на завтра: ${tomorrowFreeTable > 0}")
 }
