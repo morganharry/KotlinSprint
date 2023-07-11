ddf0ad5353272b4ca7d35e455d7eeaa0673b4d72
HM <west_ham@mail.ru>
2023-07-11 18:24:46 +0600

    KS-4-2 added const's

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,22 +1,22 @@
 package lesson_4
 
-fun main() {
-    val minWeightAverage = 35
-    val maxWeightAverage = 100
-    val maxVolumeAverage = 100
+const val MIN_WEIGHT_AVERAGE = 35
+const val MAX_WEIGHT_AVERAGE = 100
+const val MAX_VOLUME_AVERAGE = 100
 
+fun main() {
     var weightCargo = 20
     var volumeCargo = 80
     println(
         "Груз с весом ${weightCargo} кг и объемом ${volumeCargo} л соответствует категории 'Average': " +
-                "${weightCargo > minWeightAverage && weightCargo <= maxWeightAverage && volumeCargo < maxVolumeAverage}"
+                "${weightCargo > MIN_WEIGHT_AVERAGE && weightCargo <= MAX_WEIGHT_AVERAGE && volumeCargo < MAX_VOLUME_AVERAGE}"
     )
 
     weightCargo = 50
     volumeCargo = 100
     println(
         "Груз с весом ${weightCargo} кг и объемом ${volumeCargo} л соответствует категории 'Average': " +
-                "${weightCargo > minWeightAverage && weightCargo <= maxWeightAverage && volumeCargo < maxVolumeAverage}"
+                "${weightCargo > MIN_WEIGHT_AVERAGE && weightCargo <= MAX_WEIGHT_AVERAGE && volumeCargo < MAX_VOLUME_AVERAGE}"
     )
 }
 
