c7eedb0453ad12ffc5dedf64f78bd345be917668
HM <west_ham@mail.ru>
2023-07-11 15:25:13 +0600

    KS-4-5 added variables, output in compare

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,5 +1,31 @@
 package lesson_4
 
 fun main() {
+    println("Корабль не имеет повреждений? (true/false)")
+    val damage = readln().toBoolean()
+    println("Текущий состав экипажа? (количество)")
+    val numOfTeam = readln().toInt()
+    println("Количество ящиков с провизией на борту? (количество)")
+    val numOfFoods = readln().toInt()
+    println("Благоприятность метеоусловий? (true/false)")
+    val goodWeather = readln().toBoolean()
 
+    val requiredDamageLongTrip = true
+    val minNumOfTeamLongTrip = 55
+    val maxNumOfTeamLongTrip = 70
+    val minFoodsLongTrip = 50
+
+    val NumOfTeamTrip = 70
+    val minFoodsTrip = 50
+    val requiredGoodWeatherTrip = true
+
+    when {
+        (damage == requiredDamageLongTrip && numOfTeam in (minNumOfTeamLongTrip..maxNumOfTeamLongTrip) && numOfFoods > minFoodsLongTrip) ->
+            println("Научно-исследовательский корабль может приступить к долгосрочному плаванию.")
+
+        (numOfTeam == NumOfTeamTrip && numOfFoods > minFoodsTrip && goodWeather == requiredGoodWeatherTrip) ->
+            println("Научно-исследовательский корабль может отправиться в плавание.")
+
+        else -> println("Научно-исследовательский корабль не может отправиться в плавание.")
+    }
 }
