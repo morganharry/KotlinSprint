4b34702cb7e7666edbbea4c860aa1ce564d6527f
HM <west_ham@mail.ru>
2023-07-11 14:43:38 +0600

    KS-4-3 added variables, compare in variable

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,5 +1,18 @@
 package lesson_4
 
 fun main() {
+    val requiredSun = true
+    val requiredAwning = true
+    val requiredHumidity = 20
+    val requiredSeason = "winter"
 
+    val nowSun = true
+    val nowAwning = true
+    val nowHumidity = 20
+    val nowSeason = "winter"
+
+    val check =
+        (nowSun == requiredSun && nowAwning == requiredAwning && nowHumidity == requiredHumidity && nowSeason == requiredSeason)
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $check")
 }
