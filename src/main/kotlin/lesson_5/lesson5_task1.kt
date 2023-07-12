c108b005d63ead9bdfa6e813227a2a2b60d26178
HM <west_ham@mail.ru>
2023-07-12 13:19:14 +0600

    KS-5-1 added variables with random and antibot-conditional

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,5 +1,17 @@
 package lesson_5
 
 fun main() {
+    val num1 = (0..9).random()
+    val num2 = (0..9).random()
+    val sum = num1 + num2
 
+
+    print(
+        "Для того чтобы убедиться, что вы не робот, напишите, пожалуйста, ответ на простой пример.\n" +
+                "$num1 + $num2: "
+    )
+    val userSum = readln().toInt()
+
+    if (userSum == sum) println("Добро пожаловать!")
+    else println("Доступ запрещен.")
 }
