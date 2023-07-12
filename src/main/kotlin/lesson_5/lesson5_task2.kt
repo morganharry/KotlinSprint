21d836199a5233da0b7a1a7895563a4a8d84aafb
HM <west_ham@mail.ru>
2023-07-12 13:39:12 +0600

    KS-5-2 added variables this year and users year, conditional

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,5 +1,15 @@
 package lesson_5
 
-fun main() {
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+const val AGE_OF_MAJORITY = 18
+
+fun main() {
+    val dateYear = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy")).toInt()
+    print("Введите свой год рождения: ")
+    val userYear = readln().toInt()
+
+    if ((dateYear - userYear) >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
+    else println("Вернуться на главный экран")
 }
