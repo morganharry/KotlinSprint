a40e45bcc4348e2c0dff2ee6e694a71f5fb4d765
HM <west_ham@mail.ru>
2023-07-12 14:02:41 +0600

    KS-5-3 added variables, conditional

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,5 +1,24 @@
 package lesson_5
 
-fun main() {
+import java.util.*
 
+fun main() {
+    val listWin = mutableListOf<Int>()
+    while (listWin.size != 2) {
+        val num = (1..100).random()
+        if (!listWin.contains(num)) listWin.add(num)
+    }
+
+    val scanner = Scanner(System.`in`)
+    print("Введите два числа от 1 до 100 через пробел: ")
+    val userNum1 = scanner.nextInt()
+    val userNum2 = scanner.nextInt()
+
+    when {
+        listWin.contains(userNum1) && listWin.contains(userNum2) -> println("Поздравляем! Вы выиграли главный приз!")
+        listWin.contains(userNum1) || listWin.contains(userNum2) -> println("Вы выиграли утешительный приз!")
+        else -> println("Неудача! Крутите барабан!")
+    }
+
+    println("Выигрышные числа: ${listWin.joinToString()}")
 }
