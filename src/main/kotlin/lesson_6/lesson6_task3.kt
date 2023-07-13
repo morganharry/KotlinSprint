7db09d94f0b7280e9bfc34d7dbf02190ab6a0f96
HM <west_ham@mail.ru>
2023-07-13 14:26:33 +0600

    KS-6-3 added cycles for timer with countdown

--- src/main/kotlin/lesson_6/lesson6_task3.kt
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -1,5 +1,15 @@
 package lesson_6
 
 fun main() {
+    print("Введите число, чтобы установить таймер в сек: ")
+    val timerSec = readln().toInt()
+    var counter = 0
 
+    while (true) {
+        Thread.sleep(1000)
+        counter++
+        if (counter == timerSec) break
+        println("Осталось секунд: ${timerSec - counter}")
+    }
+    println("Время вышло")
 }
