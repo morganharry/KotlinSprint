52f430aca8752071ac616c7a43f65a1189289274
harrymorgan <west_ham@mail.ru>
2023-07-13 21:25:14 +0600

    KS-6-4 replaced break to condition, replaced do to while

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -6,17 +6,13 @@ fun main() {
     val secretNum = (1..9).random()
     var counter = 0
 
-    do {
+    while (++counter <= NUM) {
         print("Введите число от 1 до 9: ")
         val userNum = readln().toInt()
         if (userNum == secretNum) {
             println("Это была великолепная игра!")
             break
         } else println("Неверно")
-        counter++
-        if (counter == NUM) {
-            println("Было загадано число $secretNum")
-            break
-        }
-    } while (true)
+    }
+    println("Было загадано число $secretNum")
 }
