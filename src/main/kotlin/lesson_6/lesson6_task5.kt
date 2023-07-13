d24e343fa2433c18dd2652ec6dfb1996637a99e0
harrymorgan <west_ham@mail.ru>
2023-07-13 21:34:12 +0600

    KS-6-5 replaced break to condition, replaced do to while

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -5,23 +5,21 @@ const val NUM_OF_TRY = 3
 fun main() {
     var counter = 0
 
-    do {
-        println("Нам важно убедиться, что вы человек. Для этого решите простой пример:")
+    while (++counter <= NUM_OF_TRY) {
         val num1 = (1..9).random()
         val num2 = (1..9).random()
         val sum = num1 + num2
+
+        println("Нам важно убедиться, что вы человек. Для этого решите простой пример:")
         print("$num1 + $num2 = ")
         val userSum = readln().toInt()
+
         if (userSum == sum) {
             println("Добро пожаловать!")
             break
-        } else {
-            counter++
-            if (counter == NUM_OF_TRY) {
-                println("Доступ запрещен")
-                break
-            }
-            println("Неверно, попробуйте еще раз")
         }
-    } while (true)
+
+        if (counter == NUM_OF_TRY) println("Доступ запрещен")
+        else println("Неверно, попробуйте еще раз")
+    }
 }
