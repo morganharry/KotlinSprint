f2cb9e3c878c02b4bcd866f671b37fced56a539a
HM <west_ham@mail.ru>
2023-07-13 14:46:42 +0600

    KS-6-5 added cycles with input and checker of arithmetic example

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -1,5 +1,27 @@
 package lesson_6
 
-fun main() {
+const val NUM_OF_TRY = 3
 
+fun main() {
+    var counter = 0
+
+    do {
+        println("Нам важно убедиться, что вы человек. Для этого решите простой пример:")
+        val num1 = (1..9).random()
+        val num2 = (1..9).random()
+        val sum = num1 + num2
+        print("$num1 + $num2 = ")
+        val userSum = readln().toInt()
+        if (userSum == sum) {
+            println("Добро пожаловать!")
+            break
+        } else {
+            counter++
+            if (counter == NUM_OF_TRY) {
+                println("Доступ запрещен")
+                break
+            }
+            println("Неверно, попробуйте еще раз")
+        }
+    } while (true)
 }
