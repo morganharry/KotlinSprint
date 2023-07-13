7bc152efc73a18d2e627a5ccaa00c9250559c8a8
HM <west_ham@mail.ru>
2023-07-13 14:21:10 +0600

    KS-6-2 added cycles for timer

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,5 +1,14 @@
 package lesson_6
 
 fun main() {
+    print("Введите число, чтобы установить таймер в сек: ")
+    val timerSec = readln().toInt()
+    var counter = 0
 
+    while (true) {
+        Thread.sleep(1000)
+        counter++
+        if (counter == timerSec) break
+    }
+    println("Прошло $timerSec секунд")
 }
