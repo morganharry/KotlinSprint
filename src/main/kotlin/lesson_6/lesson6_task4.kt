47394dac994307529b0024b0e064f2cb5b5e2ab8
HM <west_ham@mail.ru>
2023-07-13 14:35:51 +0600

    KS-6-4 added cycles with input and checker

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -1,5 +1,22 @@
 package lesson_6
 
-fun main() {
+const val NUM = 5
 
+fun main() {
+    val secretNum = (1..9).random()
+    var counter = 0
+
+    do {
+        print("Введите число от 1 до 9: ")
+        val userNum = readln().toInt()
+        if (userNum == secretNum) {
+            println("Это была великолепная игра!")
+            break
+        } else println("Неверно")
+        counter++
+        if (counter == NUM) {
+            println("Было загадано число $secretNum")
+            break
+        }
+    } while (true)
 }
