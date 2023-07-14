3dd67503ba2b6d3df578a302c8ff13efc7d44fa2
HM <west_ham@mail.ru>
2023-07-14 14:01:44 +0600

    KS-7-2 added cycle for and randomizer of number

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -1,5 +1,15 @@
 package lesson_7
 
 fun main() {
+    do {
+        val smsCode = (1000..9999).random()
 
+        println("Ваш код авторизации: $smsCode")
+        print("Введите ваш код авторизации: ")
+
+        val userCode = readln().toInt()
+
+        if (userCode == smsCode) println("Добро пожаловать.")
+        else println("Неверно.")
+    } while (userCode != smsCode)
 }
