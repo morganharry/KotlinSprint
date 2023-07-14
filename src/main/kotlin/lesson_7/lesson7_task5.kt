28c6551e6cb56d665c66e6b5306bfa0a1b4a5c29
HM <west_ham@mail.ru>
2023-07-14 14:19:51 +0600

    KS-7-5 added cycle for char

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,5 +1,13 @@
 package lesson_7
 
 fun main() {
+    println("Введите необходимую длину пароля: ")
+    val userPassSize = readln().toInt()
+    var password = ""
 
+    for (i in 1..userPassSize) {
+        val char = (('0'..'9') + ('A'..'Z') + ('a'..'z')).random()
+        password += char
+    }
+    println(password)
 }
