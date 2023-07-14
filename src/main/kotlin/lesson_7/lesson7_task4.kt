3bc91dc5a32a5f82afc3ee59e56d8a92c5e47f9a
HM <west_ham@mail.ru>
2023-07-14 14:11:40 +0600

    KS-7-4 added cycle for timer

--- src/main/kotlin/lesson_7/lesson7_task4.kt
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -1,5 +1,14 @@
 package lesson_7
 
-fun main() {
+import kotlin.concurrent.thread
 
+fun main() {
+    print("Введите количество секунд: ")
+    val userSec = readln().toInt()
+
+    for (sec in userSec downTo 1) {
+        println("Осталось секунд: $sec")
+        Thread.sleep(1000)
+    }
+    println("Время вышло")
 }
