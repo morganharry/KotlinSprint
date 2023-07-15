13d1e53d145587d6dafd010e0eb06e4c444f6d38
harrymorgan <west_ham@mail.ru>
2023-07-15 19:30:24 +0600

    KS 8-2 added array and search of element in array

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -2,5 +2,17 @@ package lesson_8
 
 
 fun main() {
+    val ingredientsArray = arrayOf("соль", "сахар", "капуста", "лук", "масло", "лимон")
 
+    print("Введите ингредиент: ")
+    val userIngredient = readln()
+
+    var isIt = false
+
+    for (i in ingredientsArray.indices) {
+        if (userIngredient == ingredientsArray[i]) isIt = true
+    }
+
+    if (isIt) println("Ингредиент $userIngredient в рецепте есть")
+    else println("Такого ингредиента в рецепте нет")
 }
