3c03027af57a7c7c4083ec7d33fae1e0892127fe
harrymorgan <west_ham@mail.ru>
2023-07-15 07:16:59 +0600

    KS 7-1 del cycle, added variables

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,10 +1,12 @@
 package lesson_7
 
 fun main() {
-    var password = ""
-    for (i in 1..6) {
-        val char = (('0'..'9') + ('a'..'z')).random()
-        password += char
-    }
-    println(password)
+    val char1 = (('0'..'9') + ('a'..'z')).random()
+    val char2 = (('0'..'9') + ('a'..'z')).random()
+    val char3 = (('0'..'9') + ('a'..'z')).random()
+    val char4 = (('0'..'9') + ('a'..'z')).random()
+    val char5 = (('0'..'9') + ('a'..'z')).random()
+    val char6 = (('0'..'9') + ('a'..'z')).random()
+
+    println(char1.toString() + char2 + char3 + char4 + char5 + char6)
 }
