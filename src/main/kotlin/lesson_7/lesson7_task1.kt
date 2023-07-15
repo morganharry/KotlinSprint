2f091e7099aaa02b089e0a05ec0cb874fe26ce63
harrymorgan <west_ham@mail.ru>
2023-07-15 15:36:09 +0600

    KS 7-1 changed ranges

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,8 +1,8 @@
 package lesson_7
 
 fun main() {
-    val rangeInt = (0..9)
     val rangeChar = ('a'..'z')
+    val rangeInt = (0..9)
     val char1 = rangeChar.random()
     val char2 = rangeInt.random()
     val char3 = rangeChar.random()
