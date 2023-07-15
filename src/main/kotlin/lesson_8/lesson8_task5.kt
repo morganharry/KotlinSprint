cb8de302a5b40b2593c54fdff04884a8ddc03aca
harrymorgan <west_ham@mail.ru>
2023-07-15 22:36:29 +0600

    KS 8-5 added empty array and cycle to add elements

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -1,6 +1,14 @@
 package lesson_8
 
-
 fun main() {
+    print("Сколько ингридиентов в вашем рецепте? ")
+    val numOfIngredient = readln().toInt()
+    var ingredientsArray = arrayOf<String>()
 
+    for (i in 0 until numOfIngredient) {
+        print("Какой ингридиент вы хотите добавить? ")
+        val userIngredientTo = readln()
+        ingredientsArray += userIngredientTo
+    }
+    println("Готово! Вы сохранили следующий список: ${ingredientsArray.joinToString(",")}.")
 }
