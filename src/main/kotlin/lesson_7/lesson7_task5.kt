8559509bad878eed3f2f038f2ccb18a5a7c80ee1
harrymorgan <west_ham@mail.ru>
2023-07-15 07:45:17 +0600

    KS 7-5 changed progression

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -5,7 +5,7 @@ fun main() {
     val userPassSize = readln().toInt()
     var password = ""
 
-    for (i in 1..userPassSize) {
+    for (i in 0 until userPassSize) {
         val char = (('0'..'9') + ('A'..'Z') + ('a'..'z')).random()
         password += char
     }
