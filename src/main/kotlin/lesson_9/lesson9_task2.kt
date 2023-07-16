62d58c95903438e1adbd9204bcb5ea16e1146b0b
harrymorgan <west_ham@mail.ru>
2023-07-16 19:01:21 +0600

    KS 9-2 added list + input

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -1,5 +1,15 @@
 package lesson_9
 
 fun main() {
+    val ingredientsList = mutableListOf("соль", "масло", "капуста")
 
+    println("В рецепте есть базовые ингредиенты: ${ingredientsList.joinToString(",")}")
+    println("Желаете добавить еще?")
+    val addition = readln()
+    if (addition == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        val userIngredient = readln()
+        ingredientsList.add(userIngredient)
+        println("Теперь в рецепте есть следующие ингредиенты: ${ingredientsList.joinToString(",")}")
+    }
 }
