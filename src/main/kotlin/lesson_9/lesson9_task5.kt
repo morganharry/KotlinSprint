265876d0b5cc2347026ca746af886ae22e2f78e1
harrymorgan <west_ham@mail.ru>
2023-07-16 22:31:46 +0600

    KS 9-5 added list and sorted output

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,5 +1,28 @@
 package lesson_9
 
-fun main() {
+import java.util.*
 
+fun main() {
+    val userIngredientList = mutableListOf<String>()
+
+    println("Введите ингредиенты.")
+    for (i in 1..5) {
+        var userIngredient = ""
+        do {
+            print("Ингредиент номер $i: ")
+            userIngredient = readln()
+        } while (userIngredient in userIngredientList)
+        userIngredientList.add(userIngredient)
+    }
+    val sortedIngredientList = userIngredientList.sorted()
+
+
+    // sortedIngredientList[0].uppercase()
+
+
+    println("${sortedIngredientList.joinToString(",")}.")
 }
+/*
+– список должен выводиться в алфавитном порядке;
+– все элементы списка выведены в одну строку, разделяя слова запятыми;
+– первое слово должно быть напечатано с заглавной буквы, в конце списка должна стоять точка.*/
