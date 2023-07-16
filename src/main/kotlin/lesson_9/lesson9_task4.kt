4c7f166e57f44b468a6f7a90b98f5645d52e2fec
harrymorgan <west_ham@mail.ru>
2023-07-16 21:47:52 +0600

    KS 9-4 added list and sorted output

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -1,5 +1,8 @@
 package lesson_9
 
 fun main() {
+    print("Введите пять наименований ингредиентов, разделяя их запятыми: ")
+    val userIngredientList = readln().split(",")
 
+    println(userIngredientList.sorted().joinToString(","))
 }
