00291bdda4e504304bdfc9e4110eeb5fbe8c4f9d
harrymorgan <west_ham@mail.ru>
2023-07-16 18:38:27 +0600

    KS 9-1 added list + output

--- src/main/kotlin/lesson_9/lesson9_task1.kt
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -1,5 +1,7 @@
 package lesson_9
 
 fun main() {
+    val ingredientsList = listOf("соль", "сахар", "масло", "капуста", "лимон", "лук")
 
+    println("В рецепте есть следующие ингредиенты:\n${ingredientsList.joinToString("\n")}")
 }
