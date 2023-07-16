654442aa1a9a692fb0f24e4e8680014183cb0b0b
harrymorgan <west_ham@mail.ru>
2023-07-16 09:41:44 +0600

    KS 8-3 changed condition

--- src/main/kotlin/lesson_8/lesson8_task3.kt
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -1,12 +1,11 @@
 package lesson_8
 
-
 fun main() {
     val ingredientsArray = arrayOf("соль", "сахар", "капуста", "лук", "масло", "лимон")
 
     print("Введите ингредиент: ")
     val userIngredient = readln()
 
-    if (ingredientsArray.contains(userIngredient)) println("Ингредиент $userIngredient в рецепте есть")
+    if (userIngredient in ingredientsArray) println("Ингредиент $userIngredient в рецепте есть")
     else println("Такого ингредиента в рецепте нет")
 }
