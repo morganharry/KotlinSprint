51233aa1d9ef95eafdecd5f37aa4cb1257217aa0
HM <west_ham@mail.ru>
2023-07-17 13:34:24 +0600

    KS-10-1 added fun move and condition who won

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -1,5 +1,23 @@
 package lesson_10
 
-fun main() {
+fun move(): Int {
+    val num = (1..6).random()
+    println("Результат: $num")
+    return num
+}
 
+fun main() {
+    println("Ход Игрока!")
+    val userNum = move()
+    println()
+
+    println("Ход Компьютера!")
+    val compNum = move()
+    println()
+
+    when {
+        userNum > compNum -> println("Победило человечество")
+        userNum < compNum -> println("Победила машина")
+        else -> println("Ничья")
+    }
 }
