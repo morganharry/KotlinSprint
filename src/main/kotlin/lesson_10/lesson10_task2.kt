2a23eea618c7091b0f5a19231d5468830fe2b4f9
HM <west_ham@mail.ru>
2023-07-17 13:51:14 +0600

    KS-10-2 added fun checker and cycle

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,5 +1,25 @@
 package lesson_10
 
-fun main() {
+const val MIN_LENGTH_LOGIN_OR_PSW = 4
 
+fun loginAndPswChecker(str: String): Boolean {
+    return if (str.length < MIN_LENGTH_LOGIN_OR_PSW) {
+        println("Логин или пароль недостаточно длинные")
+        true
+    } else {
+        println("Логин или пароль одобрены")
+        false
+    }
+}
+
+fun main() {
+    do {
+        print("Придумайте и введите логин: ")
+        val userLogin = readln()
+    } while (loginAndPswChecker(userLogin))
+
+    do {
+        print("Придумайте и введите пароль: ")
+        val userPsw = readln()
+    } while (loginAndPswChecker(userPsw))
 }
