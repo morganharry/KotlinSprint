2237d4e75e21b502ec73daaeef976d7b05be2a31
harrymorgan <west_ham@mail.ru>
2023-07-17 18:00:47 +0600

    KS 9-2 del variable, added lowercase

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -4,10 +4,9 @@ fun main() {
     val ingredientsList = mutableListOf("соль", "масло", "капуста")
 
     println("В рецепте есть базовые ингредиенты: ${ingredientsList.joinToString(",")}")
-    println("Желаете добавить еще?")
-    val addition = readln()
-    if (addition == "да") {
-        println("Какой ингредиент вы хотите добавить?")
+    print("Желаете добавить еще? ")
+    if (readln().lowercase() == "да") {
+        print("Какой ингредиент вы хотите добавить? ")
         val userIngredient = readln()
         ingredientsList.add(userIngredient)
         println("Теперь в рецепте есть следующие ингредиенты: ${ingredientsList.joinToString(",")}")
