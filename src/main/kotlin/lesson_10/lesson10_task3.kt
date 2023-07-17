7a572007539b57a10cfa20609f226f54835ad2e5
HM <west_ham@mail.ru>
2023-07-17 14:08:05 +0600

    KS-10-3 added fun generation and output

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,5 +1,19 @@
 package lesson_10
 
-fun main() {
+fun generationoOfPsw(num: Int): String {
+    var psw = ""
+    for (i in 0 until num) {
+        val char = if (i % 2 != 0) ('0'..'9').random()
+        else ("!\"#\$%&'()*+,-./ ").random()
+        psw += char
+    }
+    return psw
+}
 
+fun main() {
+    print("Введите длину пароля: ")
+    val num = readln().toInt()
+    val userPsw = generationoOfPsw(num)
+
+    println("Ваш пароль: $userPsw")
 }
