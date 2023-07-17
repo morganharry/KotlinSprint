cfd31541fa0ec77ce983e234f9d6efb8081f01a3
HM <west_ham@mail.ru>
2023-07-17 16:57:19 +0600

    KS-10-4 added fun with cycle of game

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -1,5 +1,41 @@
 package lesson_10
 
-fun main() {
+fun game(): Int {
+    var userNumWin = 0
+    do {
+        println("Ход Игрока!")
+        val userNum = move()
+        println()
 
+        println("Ход Компьютера!")
+        val compNum = move()
+        println()
+
+        when {
+            userNum > compNum -> {
+                println("Победило человечество")
+                userNumWin++
+            }
+
+            userNum < compNum -> println("Победила машина")
+
+            else -> println("Ничья")
+        }
+
+        print("Хотите бросить кости еще раз Введите Да или Нет ")
+        if (readln() == "Нет") break
+    } while (true)
+    return userNumWin
+}
+
+fun move(): Int {
+    val num = (1..6).random()
+    println("Результат: $num")
+    return num
+}
+
+fun main() {
+    val userNumWin = game()
+
+    println("Количество выйгрышных партий человека: $userNumWin")
 }
