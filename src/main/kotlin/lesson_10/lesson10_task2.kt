4de1331ccce28dfd9a7e8e06ed7fc58e086ddd18
harrymorgan <west_ham@mail.ru>
2023-07-18 06:40:31 +0600

    KS-10-2 del return

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -2,24 +2,22 @@ package lesson_10
 
 const val MIN_LENGTH = 4
 
-fun loginAndPswChecker(str: String): Boolean {
-    return if (str.length < MIN_LENGTH) {
-        println("Логин или пароль недостаточно длинные")
-        true
-    } else {
-        println("Логин или пароль одобрены")
-        false
-    }
+fun checkLoginAndPasswordLength(str: String) = if (str.length < MIN_LENGTH) {
+    println("Логин или пароль недостаточно длинные")
+    true
+} else {
+    println("Логин или пароль одобрены")
+    false
 }
 
 fun main() {
     do {
         print("Придумайте и введите логин: ")
         val userLogin = readln()
-    } while (loginAndPswChecker(userLogin))
+    } while (checkLoginAndPasswordLength(userLogin))
 
     do {
         print("Придумайте и введите пароль: ")
         val userPsw = readln()
-    } while (loginAndPswChecker(userPsw))
+    } while (checkLoginAndPasswordLength(userPsw))
 }
