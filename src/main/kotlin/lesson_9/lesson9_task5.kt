ff51cd73663a65e3fcf1a6338ea3860ef66e7000
HM <west_ham@mail.ru>
2023-07-19 13:47:15 +0600

    KS-9-5 changed from list to set

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -3,24 +3,17 @@ package lesson_9
 import java.util.*
 
 fun main() {
-    val userIngredientList = mutableListOf<String>()
+    val userIngredientList = mutableSetOf<String>()
 
     println("Введите ингредиенты.")
     for (i in 1..5) {
-        var userIngredient = ""
-        do {
-            print("Ингредиент номер $i: ")
-            userIngredient = readln()
-        } while (userIngredient in userIngredientList)
-        userIngredientList.add(userIngredient)
+        print("Ингредиент номер $i: ")
+        userIngredientList.add(readln())
     }
+
     val sortedIngredientList: MutableList<String> = userIngredientList.sorted().toMutableList()
 
-    sortedIngredientList[0] = sortedIngredientList[0].replaceFirstChar {it.titlecase(Locale.getDefault())}
+    sortedIngredientList[0] = sortedIngredientList[0].replaceFirstChar { it.titlecase(Locale.getDefault()) }
 
     println("${sortedIngredientList.joinToString(",")}.")
 }
-/*
-– список должен выводиться в алфавитном порядке;
-– все элементы списка выведены в одну строку, разделяя слова запятыми;
-– первое слово должно быть напечатано с заглавной буквы, в конце списка должна стоять точка.*/
