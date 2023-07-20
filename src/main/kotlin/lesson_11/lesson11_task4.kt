c9c3f952a3b311ac1a3b47478a74a74e59953501
harrymorgan <west_ham@mail.ru>
2023-07-20 20:11:38 +0600

    KS-11-4 added class (properties, methods)

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,6 +1,15 @@
 package lesson_11
 
 fun main() {
-
-
+    var contact1 = Contact(
+        "myavatar.jpg",
+        "First name",
+        "Second name",
+        "8 (999) 999-42-42",
+        "8 (999) 888-42-42",
+        "mail@mail.ru",
+        Contact(firstName = "жена", secondName = "username"),
+        Contact(firstName = "подруга", secondName = "username"),
+        Contact(firstName = "подруга", secondName = "username"),
+    )
 }
