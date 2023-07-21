c27a39609da4e9f754f3b4dde773210c32e7d65a
HM <west_ham@mail.ru>
2023-07-21 09:48:42 +0600

    KS-11-3 added class description to code file

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -1,14 +1,39 @@
 package lesson_11
 
-fun main() {
-    val user1 = Users("North1955", "пользователь заглушен")
-    val user2 = Users("South_1313")
-    val user3 = Users("WestTT")
-    val user4 = Users("East777", "разговаривает")
+class Rooms(
+    val cover: String,
+    val name: String,
+    val usersList: MutableSet<UsersCl>,
+) {
+    fun roomView():String {
+        val loginsList = mutableListOf<String>()
+        usersList.forEach() {
+            loginsList.add(it.login)
+        }
+        return ("\t- обложка: $cover, название: $name, участники: ${loginsList.joinToString(",")}")
+    }
+}
 
-    val room1 = Rooms("Smile.jpg", "jokes", mutableSetOf<Users>(user1, user3))
-    val room2 = Rooms("hockey.jpg", "NHL-2023", mutableSetOf<Users>(user2, user3, user4))
-    val room3 = Rooms("film_reel.jpg", "cinema", mutableSetOf<Users>(user1, user2, user3, user4))
+class UsersCl (
+    val login:String,
+    var status:String = "микрофон выключен",
+) {
+
+    fun pushOnTheAvatar () {
+        println("никнейм пользователя: $login, статус: $status")
+        println()
+    }
+}
+
+fun main() {
+    val user1 = UsersCl("North1955", "пользователь заглушен")
+    val user2 = UsersCl("South_1313")
+    val user3 = UsersCl("WestTT")
+    val user4 = UsersCl("East777", "разговаривает")
+
+    val room1 = Rooms("Smile.jpg", "jokes", mutableSetOf<UsersCl>(user1, user3))
+    val room2 = Rooms("hockey.jpg", "NHL-2023", mutableSetOf<UsersCl>(user2, user3, user4))
+    val room3 = Rooms("film_reel.jpg", "cinema", mutableSetOf<UsersCl>(user1, user2, user3, user4))
 
     val listOfRooms = mutableSetOf<Rooms>(room1, room2, room3)
 
