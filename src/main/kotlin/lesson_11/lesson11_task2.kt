f66826c8f6af1ef988cc4895272686495decaf0c
HM <west_ham@mail.ru>
2023-07-21 09:49:46 +0600

    KS-11-2 added class description to code file

--- src/main/kotlin/lesson_11/lesson11_task2.kt
+++ src/main/kotlin/lesson_11/lesson11_task2.kt
@@ -1,7 +1,45 @@
 package lesson_11
 
+class Users(
+    val id: Int,
+    val login: String,
+    var psw: String,
+    val mail: String,
+    var bio: String = "",
+
+    ) {
+    fun printUser() {
+        println("id: ${String.format("%06d", id)}, login: ${login}, password: ${psw}, mail: ${mail}, bio: ${bio}")
+        println()
+    }
+
+    fun addBio() {
+        print("Введите информацию \"о себе\":")
+        bio = readln()
+        println()
+    }
+
+    fun changePsw() {
+        do {
+            print("Введите текущий пароль: ")
+            if (readln() == psw) break
+            else println("Пароль неверный!")
+        } while (true)
+        print("Введите новый пароль: ")
+        psw = readln()
+        println("Новый пароль установлен.")
+        println()
+    }
+
+    fun sendToUser() {
+        print("Введите сообщение для пользователя ${login}: ")
+        val message = readln()
+        println("${login}, вам сообщение:\n\"${message}\"")
+        println()
+    }
+}
 fun main() {
-    val user1 = User(234, "Fedor", "123456789", "fedor1981@gmail.com")
+    val user1 = Users(234, "Fedor", "123456789", "fedor1981@gmail.com")
     do {
         print(
             "Для выполнения задачи введите соотвествующую цифру:\n" +
