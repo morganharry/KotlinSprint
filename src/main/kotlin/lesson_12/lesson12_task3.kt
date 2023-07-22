77e913e19a265067069c62f8e8412c6b3b05f40c
harrymorgan <west_ham@mail.ru>
2023-07-22 18:14:27 +0600

    KS-12-3 created class with properties

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,6 +1,19 @@
 package lesson_12
 
+class WeatherInfo(var dayTemp: Int, var nightTemp: Int, var wasItRaining: Boolean = false, var atmospherePressure: Int) {
+    fun weatherView() {
+        println("Дневная тепература: $dayTemp°\nНочная температура: $nightTemp°\nОсадки: $wasItRaining\nАтм. давление: $atmospherePressure мм рт. ст.")
+        println()
+    }
+}
+
 fun main() {
+    val day1 = WeatherInfo(41, 25,  atmospherePressure = 755)
+    day1.weatherView()
 
+    val day2 = WeatherInfo(31, 27, true, 754)
+    day2.weatherView()
 
+    val day3 = WeatherInfo(37, 22,  atmospherePressure = 755)
+    day3.weatherView()
 }
