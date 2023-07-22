45d23f7a845024f3363adbf909ff22750cc770be
harrymorgan <west_ham@mail.ru>
2023-07-22 17:17:25 +0600

    KS-11-3 changed collection from Set to List, changed output of the collection

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -3,17 +3,18 @@ package lesson_11
 class Room(
     val cover: String,
     val name: String,
-    val usersList: Set <User>,
+    val usersList: List<User>,
 ) {
-    fun roomView():String =("\t- обложка: $cover, название: $name, участники: ${usersList.joinToString(",")}")
+    fun roomView(): String =
+        ("\t- обложка: $cover, название: $name, участники: ${usersList.joinToString(", ") { it.login }}")
 }
 
-class User (
-    val login:String,
-    var status:String = "микрофон выключен",
+class User(
+    val login: String,
+    var status: String = "микрофон выключен",
 ) {
 
-    fun pushOnTheAvatar () {
+    fun pushOnTheAvatar() {
         println("никнейм пользователя: $login, статус: $status")
         println()
     }
@@ -25,9 +26,9 @@ fun main() {
     val user3 = User("WestTT")
     val user4 = User("East777", "разговаривает")
 
-    val room1 = Room("Smile.jpg", "jokes", setOf<User>(user1, user3))
-    val room2 = Room("hockey.jpg", "NHL-2023", setOf<User>(user2, user3, user4))
-    val room3 = Room("film_reel.jpg", "cinema", setOf<User>(user1, user2, user3, user4))
+    val room1 = Room("Smile.jpg", "jokes", listOf<User>(user1, user3))
+    val room2 = Room("hockey.jpg", "NHL-2023", listOf<User>(user2, user3, user4))
+    val room3 = Room("film_reel.jpg", "cinema", listOf<User>(user1, user2, user3, user4))
 
     val listOfRooms = mutableSetOf<Room>(room1, room2, room3)
 
