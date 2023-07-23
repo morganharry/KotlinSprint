917cfafcc3cef81efe2a0d2ea18da448b563695b
harrymorgan <west_ham@mail.ru>
2023-07-23 16:10:08 +0600

    initial

--- src/main/kotlin/lesson_12/lesson12_task2.kt
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -7,7 +7,7 @@ class WeatherInf (_dayTemp: Int, _nightTemp: Int, _wasItRaining: Boolean, _atmos
     var atmospherePressure = _atmospherePressure
 
     fun weatherView() {
-        println("Дневная температура: $dayTemp°\nНочная температура: $nightTemp°\nОсадки: $wasItRaining\nАтм. давление: $atmospherePressure мм рт. ст.")
+        println("Дневная температура:$dayTemp°\nНочная температура: $nightTemp°\nОсадки: $wasItRaining\nАтм. давление: $atmospherePressure мм рт. ст.")
         println()
     }
 }
