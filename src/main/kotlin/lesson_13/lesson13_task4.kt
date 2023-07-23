966e40b6004a08a9e4e3d5aca796cc84590c15c7
harrymorgan <west_ham@mail.ru>
2023-07-23 12:01:30 +0600

    KS-13-4 added method for adding new contacts

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -1,6 +1,43 @@
 package lesson_13
 
+class ContactByPhone(var name: String?, var phoneNum: Long, var company: String?) {
+}
+
+fun contactListView(list: MutableList<ContactByPhone>) {
+    println("Контакты:")
+    for (contact in list) {
+        println("- Имя: ${contact.name} Номер: ${contact.phoneNum} Компания: ${contact.company}")
+    }
+}
+
+fun addContact(list: MutableList<ContactByPhone>): MutableList<ContactByPhone> {
+    do {
+        print("Введите имя контакта: ")
+
+        var name: String? = readln()
+        if (name == "") name = null
+        print("Введите номер телефона контакта: ")
+
+        val phoneNum = readln()
+        if (phoneNum == "") println("Поле не может быть пустым")
+        else {
+            print("Введите название компании контакта: ")
+
+            var company: String? = readln()
+            if (company == "") company = null
+
+            list.add(ContactByPhone(name, phoneNum.toLong(), company))
+        }
+        println()
+        println("Если хотите добавить новую запись, введите \"да\"")
+        if (readln().lowercase() != "да") break
+    } while (true)
+    return list
+}
+
 fun main() {
+    var contactList = mutableListOf<ContactByPhone>()
 
-
+    contactList = addContact(contactList)
+    contactListView(contactList)
 }
