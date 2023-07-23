9c95e9abf06a0e5f91cd6d6a7bc095dca7c6e056
harrymorgan <west_ham@mail.ru>
2023-07-23 15:14:36 +0600

    initial

---
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -0,0 +1,30 @@
+package lesson_12
+
+class Weather() {
+    var dayTemp: Int = 0
+    var nightTemp: Int = 0
+    var wasItRaining: Boolean = false
+    var atmospherePressure: Int = 0
+
+    fun weatherView() {
+        println("Дневная температура: $dayTemp°\nНочная температура: $nightTemp°\nОсадки: $wasItRaining\nАтм. давление: $atmospherePressure мм рт. ст.")
+        println()
+    }
+}
+
+fun main() {
+    val day1 = Weather()
+    day1.dayTemp = 39
+    day1.nightTemp = 21
+    day1.atmospherePressure = 755
+
+    day1.weatherView()
+
+    val day2 = Weather()
+    day2.dayTemp = 37
+    day2.nightTemp = 25
+    day2.wasItRaining = true
+    day2.atmospherePressure = 756
+
+    day2.weatherView()
+}
