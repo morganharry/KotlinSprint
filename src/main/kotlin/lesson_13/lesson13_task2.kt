e3f2dbea6021b7f00ab4df90560982a964cffb40
harrymorgan <west_ham@mail.ru>
2023-07-23 08:21:34 +0600

    KS-13-2 added elvis-op in output method

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -1,6 +1,15 @@
 package lesson_13
 
+class PhoneContact (var name: String, var phoneNum: Long, var company: String?) {
+    fun contactView() {
+        println("Имя: $name Номер: $phoneNum Компания: ${company ?: "не указано"}")
+    }
+}
+
 fun main() {
-
-
+    val contact1 = PhoneContact("Vasiliy", 89999999999, "AirLines")
+    val contact2 = PhoneContact("Igor", 89999999998, null)
+    println("Контакты:")
+    contact1.contactView()
+    contact2.contactView()
 }
