f34a56869cd3b65db2288cf753b5278ef1942489
HM <west_ham@mail.ru>
2023-07-24 08:32:17 +0600

    KS-13-4 changed the name of the fun

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -1,7 +1,7 @@
 package lesson_13
 
 class PhoneContact (var name: String, var phoneNum: Long, var company: String?) {
-    fun contactView() {
+    fun showСontact() {
         println("Имя: $name Номер: $phoneNum Компания: ${company ?: "не указано"}")
     }
 }
@@ -10,6 +10,6 @@ fun main() {
     val contact1 = PhoneContact("Vasiliy", 89999999999, "AirLines")
     val contact2 = PhoneContact("Igor", 89999999998, null)
     println("Контакты:")
-    contact1.contactView()
-    contact2.contactView()
+    contact1.showСontact()
+    contact2.showСontact()
 }
