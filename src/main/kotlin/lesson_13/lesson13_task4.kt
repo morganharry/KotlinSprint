18ac260884bd3f30b21fa2c90d84d69182642ad8
harrymorgan <west_ham@mail.ru>
2023-07-25 07:00:24 +0600

    KS-13-4 changed methods for input, changed name of the fun

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -3,7 +3,7 @@ package lesson_13
 class ContactByPhone(var name: String?, var phoneNum: Long, var company: String?) {
 }
 
-fun contactListView(list: MutableList<ContactByPhone>) {
+fun showContactList(list: MutableList<ContactByPhone>) {
     println("Контакты:")
     for (contact in list) {
         println("- Имя: ${contact.name} Номер: ${contact.phoneNum} Компания: ${contact.company}")
@@ -15,22 +15,22 @@ fun addContact(list: MutableList<ContactByPhone>): MutableList<ContactByPhone> {
         print("Введите имя контакта: ")
 
         var name: String? = readln()
-        if (name == "") name = null
+        if (name.isNullOrEmpty()) name = null
         print("Введите номер телефона контакта: ")
 
         val phoneNum = readln()
-        if (phoneNum == "") println("Поле не может быть пустым")
+        if (phoneNum.isNullOrEmpty()) println("Поле не может быть пустым")
         else {
             print("Введите название компании контакта: ")
 
             var company: String? = readln()
-            if (company == "") company = null
+            if (company.isNullOrEmpty()) company = null
 
             list.add(ContactByPhone(name, phoneNum.toLong(), company))
         }
         println()
         println("Если хотите добавить новую запись, введите \"да\"")
-        if (readln().lowercase() != "да") break
+        if (!readln().equals("да")) break
     } while (true)
     return list
 }
@@ -39,5 +39,5 @@ fun main() {
     var contactList = mutableListOf<ContactByPhone>()
 
     contactList = addContact(contactList)
-    contactListView(contactList)
+    showContactList(contactList)
 }
