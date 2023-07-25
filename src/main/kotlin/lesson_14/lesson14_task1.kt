40ed3368db13bb5453cbc9236618640e0548ef64
HM <west_ham@mail.ru>
2023-07-25 10:25:12 +0600

    KS-14-1 added class with properties amd fun's

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,5 +1,22 @@
 package lesson_14
 
-fun main() {
+class Ship(
+    val name: String,
+    val speed: Int,
+    val capacity: Int,
+) {
+    fun showSpeed() {
+        println("Скорость корабля $name: $speed")
+    }
 
+    fun showCapacity() {
+        println("Груза на корабле $name: $capacity")
+    }
+}
+
+fun main() {
+    val ship1 = Ship("Hope", 500, 1000)
+
+    ship1.showSpeed()
+    ship1.showCapacity()
 }
