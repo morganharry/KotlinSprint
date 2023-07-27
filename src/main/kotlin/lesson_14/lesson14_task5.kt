479bad0c64ca8f80c9179d8a54282bccc646dfb9
harrymorgan <west_ham@mail.ru>
2023-07-27 06:49:08 +0600

    KS-14-5 added enum

--- src/main/kotlin/lesson_14/lesson14_task5.kt
+++ src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -2,78 +2,76 @@ package lesson_14
 
 import kotlin.math.*
 
-const val NUM_RAD_IN_DIAMETER = 2
-abstract class Figure(
-    val color: String,
-) {
-    open fun countPerimeter():Int  {
-        val perimeter = 0
-        println(perimeter)
-        return perimeter
-    }
+enum class Colors(val color: String) {
+    GREEN("green"), RED("red"), BLACK("black"),
+}
 
-    open fun countSquare():Int  {
-        val square = 0
-        println(square)
-        return square
-    }
+const val NUM_RAD_IN_DIAMETER = 2
+
+abstract class Figure(
+    val colorOfFigure: String,
+) {
+    abstract fun countPerimeter(): Int
+
+    abstract fun countSquare(): Int
 }
 
 class Circle(
-    color: String,
+    colorOfFigure: String,
     val radius: Int,
-) : Figure(color) {
-    override fun countPerimeter():Int {
-        val perimeter =  (NUM_RAD_IN_DIAMETER * radius * PI).toInt()
+) : Figure(colorOfFigure) {
+    override fun countPerimeter(): Int {
+        val perimeter = (NUM_RAD_IN_DIAMETER * radius * PI).toInt()
         return perimeter
     }
 
-    override fun countSquare():Int  {
-        val square =(radius * radius * PI).toInt()
+    override fun countSquare(): Int {
+        val square = (radius * radius * PI).toInt()
         return square
     }
 }
 
-class Rectangle (
-    color: String,
+class Rectangle(
+    colorOfFigure: String,
     val sideA: Int,
     val sideB: Int,
-) : Figure(color) {
-    override fun countPerimeter():Int {
-        val perimeter =  sideA + sideB + sideA + sideB
+) : Figure(colorOfFigure) {
+    override fun countPerimeter(): Int {
+        val perimeter = sideA + sideB + sideA + sideB
         return perimeter
     }
 
-    override fun countSquare():Int  {
+    override fun countSquare(): Int {
         val square = sideA * sideB
         return square
     }
 }
 
 class Triangle(
-    color: String,
+    colorOfFigure: String,
     val sideA: Int,
     val sideB: Int,
     val sideC: Int,
-) : Figure(color) {
-    override fun countPerimeter():Int {
-        val perimeter =  sideA + sideB + sideC
+) : Figure(colorOfFigure) {
+    override fun countPerimeter(): Int {
+        val perimeter = sideA + sideB + sideC
         return perimeter
     }
 
-    override fun countSquare():Int  {
-        val square = sqrt((countPerimeter()*(countPerimeter()-sideA)*(countPerimeter()-sideB)*(countPerimeter()-sideC)).toDouble()).toInt()
+    override fun countSquare(): Int {
+        val square =
+            sqrt((countPerimeter() * (countPerimeter() - sideA) * (countPerimeter() - sideB) * (countPerimeter() - sideC)).toDouble()).toInt()
         return square
     }
 }
 
 fun main() {
-    val circle1 = Circle("red", 10)
-    val circle2 = Circle("red", 20)
-    val rectangle1 = Rectangle("red", 10, 20)
-    val rectangle2 = Rectangle("red", 25, 35)
-    val triangle1 = Triangle("green", 6, 8, 10)
-    val triangle2 = Triangle("red", 12, 16, 20)
+    val circle1 = Circle(Colors.RED.color, 10)
+    val circle2 = Circle(Colors.RED.color, 20)
+    val rectangle1 = Rectangle(Colors.GREEN.color, 10, 20)
+    val rectangle2 = Rectangle(Colors.BLACK.color, 25, 35)
+    val triangle1 = Triangle(Colors.GREEN.color, 6, 8, 10)
+    val triangle2 = Triangle(Colors.RED.color, 12, 16, 20)
     val listOfFigure = mutableListOf<Figure>()
 
     listOfFigure.add(circle1)
@@ -82,27 +80,28 @@ fun main() {
     listOfFigure.add(rectangle2)
     listOfFigure.add(triangle1)
     listOfFigure.add(triangle2)
-    println (listOfFigure.map { it.color }.joinToString(", "))
+    println(listOfFigure.map { it.colorOfFigure }.joinToString(", "))
 
     print("Выберите цвет фигур для расчета периметра (red/black/green): ")
-    val colorForPer = readln()
-    if (colorForPer == "red" ||colorForPer == "black" ||colorForPer == "green")
-        println("Периметр всех фигур цвета $colorForPer равен: ${countAllFigWithColor (colorForPer, listOfFigure, true)}.")
+    val colorForPer = readln().lowercase()
+    if (colorForPer == Colors.RED.color || colorForPer == Colors.GREEN.color || colorForPer == Colors.BLACK.color) println(
+        "Периметр всех фигур цвета $colorForPer равен: ${countAllFigWithColor(colorForPer, listOfFigure, true)}."
+    )
     else println("Вы ввели неправильный цвет.")
 
     print("Выберите цвет фигур для расчета площади (red/black/green): ")
-    val colorForSquare = readln()
-    if (colorForSquare == "red" ||colorForSquare == "black" ||colorForSquare == "green")
-        println("Площадь всех фигур цвета $colorForSquare равна: ${countAllFigWithColor (colorForSquare, listOfFigure, false)}.")
+    val colorForSquare = readln().lowercase()
+    if (colorForPer == Colors.RED.color || colorForPer == Colors.GREEN.color || colorForPer == Colors.BLACK.color) println(
+        "Площадь всех фигур цвета $colorForSquare равна: ${countAllFigWithColor(colorForSquare, listOfFigure, false)}."
+    )
     else println("Вы ввели неправильный цвет.")
 }
 
-fun countAllFigWithColor (color:String, list:MutableList<Figure>, per:Boolean):Int {
+fun countAllFigWithColor(color: String, list: MutableList<Figure>, isItPer: Boolean): Int {
     var sumAllFig = 0
     for (figure in list) {
-        if (figure.color == color)
-            if (per) sumAllFig += figure.countPerimeter()
-            else sumAllFig += figure.countSquare()
+        if (figure.colorOfFigure == color) if (isItPer) sumAllFig += figure.countPerimeter()
+        else sumAllFig += figure.countSquare()
     }
     return sumAllFig
 }
