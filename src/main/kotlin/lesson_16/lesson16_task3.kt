e82d3f4d6b79992117038da3ea3db1e350896379
harrymorgan <west_ham@mail.ru>
2023-07-29 20:35:50 +0600

    KS-16-3 added class with private val

--- src/main/kotlin/lesson_16/lesson16_task3.kt
+++ src/main/kotlin/lesson_16/lesson16_task3.kt
@@ -1,5 +1,21 @@
 package lesson_16
 
-fun main() {
+class User(val name: String = "User2023", private val password: String = "Qwerty") {
 
+    fun checkPsw() {
+        do {
+            print("Введите пароль от аккаунта $name: ")
+            if (readln() == password) {
+                println("Пароль совпал!")
+                break
+            } else println("Увы.")
+            print("Еще одна попытка? (да/нет): ")
+            if (readln().equals("нет", true)) break
+        } while (true)
+    }
+}
+
+fun main() {
+    val user1 = User()
+    user1.checkPsw()
 }
