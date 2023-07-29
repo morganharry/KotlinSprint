497eb1ed34d2475716ae2bfd9cdf034fce76e14d
harrymorgan <west_ham@mail.ru>
2023-07-29 08:05:02 +0600

    KS-16-2 added private class with private val

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -1,5 +1,24 @@
 package lesson_16
 
-fun main() {
+const val NUM_FOR_PERIMETER = 2
 
+private class Circle(private val radius: Int) {
+    val pi = 3.14
+    fun square() {
+        val squareOfCircle = (pi * radius * radius).toInt()
+        println("Площадь круга с радиусом $radius равна: $squareOfCircle")
+    }
+
+    fun perimeter() {
+        val perimeterOfCircle = (NUM_FOR_PERIMETER * pi * radius).toInt()
+        println("Длина окружности с радиусом $radius равна: $perimeterOfCircle")
+    }
+}
+
+fun main() {
+    print("Введите радиус окружности: ")
+    val circle1 = Circle(readln().toInt())
+    
+    circle1.square()
+    circle1.perimeter()
 }
