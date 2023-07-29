6f5c58c238faf3b452ddb8a81a210e199feff634
harrymorgan <west_ham@mail.ru>
2023-07-29 21:08:35 +0600

    KS-16-4 added class with private var

--- src/main/kotlin/lesson_16/lesson16_task4.kt
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -1,5 +1,28 @@
 package lesson_16
 
-fun main() {
+class Order(val num: Int, private var status: String) {
 
+    fun printInfo() {
+        println("Заказ $num в статусе $status")
+    }
+
+    fun changeNum(_status: String) {
+        status = _status
+    }
+}
+
+fun main() {
+    val order1 = Order(937, "на сборке")
+    order1.printInfo()
+    changeNumOrder(order1)
+    order1.printInfo()
+}
+
+fun changeNumOrder(order1: Order) {
+    print("Для смены статуса заказа ${order1.num} необходимо направить заявку менеджеру. Направить? (да/нет): ")
+    if (readln().equals("да", true)) {
+        print("Какой статус приствоить заказу ${order1.num}? ")
+        val status = readln()
+        order1.changeNum(status)
+    }
 }
