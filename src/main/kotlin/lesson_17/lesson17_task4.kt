89d9fbfb7ba68e2b0f377931073e20bda116ee5d
harrymorgan <west_ham@mail.ru>
2023-07-30 19:25:12 +0600

    KS-17-4 added class with set in property

--- src/main/kotlin/lesson_17/lesson17_task4.kt
+++ src/main/kotlin/lesson_17/lesson17_task4.kt
@@ -1,5 +1,25 @@
 package lesson_17
 
-fun main() {
+class Package(val num: Int, _place: String, var count: Int = 0) {
+    var place = _place
+        set(value) {
+            field = value
+            count++
+        }
 
+    fun setNewPlace() {
+        print("Укажите новое местоположение посылки №$num: ")
+        place = readln()
+    }
+
+    fun getInfo() {
+        println("Посылка №$num, местоположение $place, счётчик:$count. ")
+    }
+}
+
+fun main() {
+    val package1 = Package(45, "Москва")
+    package1.getInfo()
+    package1.setNewPlace()
+    package1.getInfo()
 }
