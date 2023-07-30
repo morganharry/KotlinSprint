dfe41aa4b8140a2b81f901069b465e90c5ac0b8e
harrymorgan <west_ham@mail.ru>
2023-07-30 19:11:41 +0600

    KS-17-3 added class with get in property

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -1,5 +1,21 @@
 package lesson_17
 
-fun main() {
+class Directory(_name: String, _numOfFiles: Int, _isSecret: Boolean) {
+    var name: String = _name
+        get() = if (isSecret == true) "скрытая папка" else field
+    var numOfFiles: Int = _numOfFiles
+        get() = if (isSecret == true) 0 else field
+    var isSecret: Boolean = _isSecret
 
+    fun getInfo() {
+        println("Папка \"$name\" содержит $numOfFiles файлов.")
+    }
+}
+
+fun main() {
+    val directory1 = Directory("презентации", 5, false)
+    val directory2 = Directory("отчёты", 10, true)
+
+    directory1.getInfo()
+    directory2.getInfo()
 }
