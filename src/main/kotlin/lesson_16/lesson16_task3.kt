51888127577113e05b4d6d643e82396e96e4234f
harrymorgan <west_ham@mail.ru>
2023-07-30 15:23:41 +0600

    KS-16-3 changed fun in class, variable declaration

--- src/main/kotlin/lesson_16/lesson16_task3.kt
+++ src/main/kotlin/lesson_16/lesson16_task3.kt
@@ -1,21 +1,21 @@
 package lesson_16
 
-class User(val name: String = "User2023", private val password: String = "Qwerty") {
+class User(val name: String, private val password: String) {
 
-    fun checkPsw() {
-        do {
-            print("Введите пароль от аккаунта $name: ")
-            if (readln() == password) {
-                println("Пароль совпал!")
-                break
-            } else println("Увы.")
-            print("Еще одна попытка? (да/нет): ")
-            if (readln().equals("нет", true)) break
-        } while (true)
+    fun checkPsw(_password: String): Boolean {
+        return (_password == password)
     }
 }
 
 fun main() {
-    val user1 = User()
-    user1.checkPsw()
+    val user1 = User("User2023", "Qwerty")
+    do {
+        print("Введите пароль от аккаунта ${user1.name}: ")
+        if (user1.checkPsw(readln())) {
+            println("Пароль совпал!")
+            break
+        } else println("Увы.")
+        print("Еще одна попытка? (да/нет): ")
+        if (!readln().equals("да", true)) break
+    } while (true)
 }
