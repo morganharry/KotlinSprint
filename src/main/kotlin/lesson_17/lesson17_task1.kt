52233a05cd7a91fba9351508d16eb94961cd8fe0
HM <west_ham@mail.ru>
2023-07-31 14:18:50 +0600

    KS-17-1 added parameters in primary constructor, input check in main

--- src/main/kotlin/lesson_17/lesson17_task1.kt
+++ src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -3,21 +3,23 @@ package lesson_17
 class Quiz(
     _question: String = "Что было создано благодаря грибам-плесени?",
     _answer: String = "Пенициллин",
-    var isTrue: Boolean = false
 ) {
     val question: String = _question
         get() = field
     var answer: String = _answer
         get() = field
         set(value) {
-            if (value.equals(field, true)) isTrue = true
+            field = value
         }
 }
 
 fun main() {
     val quiz1 = Quiz()
     println(quiz1.question)
-    quiz1.answer = readln()
-    println("Ваш ответ: ${quiz1.isTrue}")
-    println("Правильный ответ: ${quiz1.answer}")
+
+    val answer = readln()
+
+    if (answer.equals(quiz1.answer, true)) println("Это правильный ответ!")
+    else println("Увы, но правильный ответ: ${quiz1.answer}")
+    quiz1.answer = answer
 }
