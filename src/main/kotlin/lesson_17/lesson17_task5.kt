1bb35d75bbf00be63f4910d4d136ab4a3bf80cda
HM <west_ham@mail.ru>
2023-07-31 16:40:23 +0600

    KS-17-5 added setter and getter in properties

--- src/main/kotlin/lesson_17/lesson17_task5.kt
+++ src/main/kotlin/lesson_17/lesson17_task5.kt
@@ -1,5 +1,33 @@
 package lesson_17
 
-fun main() {
 
+class User(
+    _login: String,
+    _psw: String,
+) {
+    var login: String = _login
+        get() = field
+        set(value) {
+            field = value
+            println("Вы изменили логин на $field")
+        }
+    var psw: String = _psw
+        get() {
+            var pswSecret = ""
+            repeat(field.length) { pswSecret += "*" }
+            return (pswSecret)
+        }
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+}
+
+fun main() {
+    val user1 = User("Boris1985", "123456")
+    print("Введите новый логин: ")
+    user1.login = readln()
+    print("Введите новый пароль: ")
+    user1.psw = readln()
+    print("Действующий пароль: ${user1.psw}")
+    user1.psw
 }
