36ce14e9ac5c16c88d2b6274275127270886cce9
HM <west_ham@mail.ru>
2023-08-01 13:28:55 +0600

    KS-18-4 added classes with fun's

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -1,5 +1,29 @@
 package lesson_18
 
-fun main() {
+const val NUM_DUPLICATE_RECTANGLE = 2
+const val NUM_DUPLICATE_CUBE = 6
 
+open class Package(val sizeA: Int) {
+    open fun getSquarePackage() {
+        val square = NUM_DUPLICATE_CUBE * sizeA * sizeA
+        println("Площадь поверхности посылки: $square")
+    }
+}
+
+class PackageRectangular(sizeA: Int, val sizeB: Int, val sizeC: Int) : Package(sizeA) {
+    override fun getSquarePackage() {
+        val square = NUM_DUPLICATE_RECTANGLE * (sizeA * sizeB + sizeA * sizeC + sizeB * sizeC)
+        println("Площадь поверхности посылки: $square")
+    }
+}
+
+fun main() {
+    val package1: Package = PackageRectangular(1, 1, 2)
+    val package2 = Package(1)
+
+    val listOfPackage = listOf<Package>(package1, package2)
+
+    listOfPackage.forEach {
+        it.getSquarePackage()
+    }
 }
