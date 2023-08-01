a520fbe09377ea7e08a04e172212ee3c0b797292
harrymorgan <west_ham@mail.ru>
2023-08-01 19:38:39 +0600

    KS-18-4 added class, changed primary class to abstract

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -3,14 +3,18 @@ package lesson_18
 const val NUM_DUPLICATE_RECTANGLE = 2
 const val NUM_DUPLICATE_CUBE = 6
 
-open class Package(val sizeA: Int) {
-    open fun getSquarePackage() {
+abstract class Package() {
+    abstract fun getSquarePackage()
+}
+
+class PackageCube(val sizeA: Int) : Package() {
+    override fun getSquarePackage() {
         val square = NUM_DUPLICATE_CUBE * sizeA * sizeA
         println("Площадь поверхности посылки: $square")
     }
 }
 
-class PackageRectangular(sizeA: Int, val sizeB: Int, val sizeC: Int) : Package(sizeA) {
+class PackageRectangular(val sizeA: Int, val sizeB: Int, val sizeC: Int) : Package() {
     override fun getSquarePackage() {
         val square = NUM_DUPLICATE_RECTANGLE * (sizeA * sizeB + sizeA * sizeC + sizeB * sizeC)
         println("Площадь поверхности посылки: $square")
@@ -19,7 +23,7 @@ class PackageRectangular(sizeA: Int, val sizeB: Int, val sizeC: Int) : Package(s
 
 fun main() {
     val package1: Package = PackageRectangular(1, 1, 2)
-    val package2 = Package(1)
+    val package2: Package = PackageCube(1)
 
     val listOfPackage = listOf<Package>(package1, package2)
 
