265ec100af26eab3b51313ab8a88955a6a903a79
HM <west_ham@mail.ru>
2023-08-01 12:56:08 +0600

    technical changed

--- src/main/kotlin/lesson_16/lesson16_task5.kt
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -1,15 +1,4 @@
 package lesson_16
 
-class Dice(
-    val name: String,
-    private val num: Int = (1..6).random(),
-) {
-    fun showResult() {
-        println("Результат: $num.")
-    }
-}
-
 fun main() {
-    val dice = Dice("dice1")
-    dice.showResult()
 }
