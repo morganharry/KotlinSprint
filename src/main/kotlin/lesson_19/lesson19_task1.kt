c0ef5fdd0b2f8e1ffdeee05322ea19776e2d9849
HM <west_ham@mail.ru>
2023-08-02 08:42:47 +0600

    KS-19-1 added enum class with fun's

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,5 +1,35 @@
 package lesson_19
 
-fun main() {
+const val INDEX_CORRECTION = 1
 
+enum class Fish {
+    GUPPY {
+        override fun getNameInRus(): String {
+            return "Гуппи"
+        }
+    },
+    ANGELFISH {
+        override fun getNameInRus(): String {
+            return "Скалярия"
+        }
+    },
+    GOLDFISH {
+        override fun getNameInRus(): String {
+            return "Золотая рыбка"
+        }
+    },
+    SIAMESE_FIGHTING_FISH {
+        override fun getNameInRus(): String {
+            return "Петушок"
+        }
+    };
+
+    abstract fun getNameInRus(): String
+}
+
+
+fun main() {
+    println("Вы можете добавить в акваруим:")
+
+    for (fish in Fish.values()) println("${fish.ordinal + INDEX_CORRECTION} - ${fish.getNameInRus()}")
 }
