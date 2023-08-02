3f9d82cd577386b3a7f46877d8c1322d2555c079
HM <west_ham@mail.ru>
2023-08-02 16:50:40 +0600

    KS-19-4 added enum and class with fun's

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -1,5 +1,37 @@
 package lesson_19
 
-fun main() {
+enum class Ammo(val damage: Int, val color: String) {
+    BLUE(5, "Синий"),
+    GREEN(10, "Зелёный"),
+    RED(20, "Красный"),
+}
 
+class Tank(val id: Int, var ammo: Ammo) {
+    fun changeAmmo() {
+        do {
+            println("Доступные варианты патронов:")
+            for (ammo in Ammo.values()) println("- \"${ammo.color}\" (урон: ${ammo.damage})")
+            print("Введите выбранный тип: ")
+            when (readln()) {
+                "Синий" -> ammo = Ammo.BLUE
+                "Зелёный" -> ammo = Ammo.GREEN
+                "Красный" -> ammo = Ammo.RED
+                else -> println("Указан некорректный тип.")
+            }
+            println("Заряжен патрон типа: \"${ammo.color}\" (урон: ${ammo.damage}).")
+            print("Хотите поменять тип патрона? (да/нет): ")
+        } while (readln().equals("да", true))
+    }
+
+    fun fire() {
+        println("Произведен выстрел патроном типа \"${ammo.color}\".")
+        println("Урон: ${ammo.damage}!")
+    }
+}
+
+fun main() {
+    val tank1 = Tank(42, Ammo.BLUE)
+    tank1.fire()
+    tank1.changeAmmo()
+    tank1.fire()
 }
