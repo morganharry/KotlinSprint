384760d878671f50b02a629dd092d63d833e0299
harrymorgan <west_ham@mail.ru>
2023-08-03 23:26:57 +0600

    KS-20-2 added class and lambda

--- src/main/kotlin/lesson_20/lesson20_task2.kt
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -1,5 +1,17 @@
 package lesson_20
 
+const val MAX_HEALTH = 100
+
+class Player(val name: String, var health: Int)
+
 fun main() {
+    val player1 = Player("Larry", 70)
+    println("Иду-иду...бац! Ух ты, волшебное зелье! Как неожиданно и приятнооо!");
+
+    run {
+        if (player1.health < MAX_HEALTH) player1.health = MAX_HEALTH
+        println("Будь здоров! ${player1.health}")
+    }
+
 
 }
