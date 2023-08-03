e0634c80f2ebb951aa63cfac8b2333b51a6def9a
HM <west_ham@mail.ru>
2023-08-03 12:10:05 +0600

    KS-20-1 changed enum

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -2,7 +2,7 @@ package lesson_19
 
 const val INDEX_CORRECTION = 1
 
-enum class Fish(val nameInRus: String) {
+enum class Fish(val fishRus: String) {
     GUPPY("Гуппи"),
     ANGELFISH("Скалярия"),
     GOLDFISH("Золотая рыбка"),
@@ -12,5 +12,5 @@ enum class Fish(val nameInRus: String) {
 fun main() {
     println("Вы можете добавить в акваруим:")
 
-    for (fish in Fish.values()) println("${fish.ordinal + INDEX_CORRECTION} - ${fish.nameInRus}")
+    for (fish in Fish.values()) println("${fish.ordinal + INDEX_CORRECTION} - ${fish.fishRus}")
 }
