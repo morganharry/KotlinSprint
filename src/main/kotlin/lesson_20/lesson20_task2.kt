53a73db572b4c2f1b18ee7d9efc8f40ac01aae16
HM <west_ham@mail.ru>
2023-08-04 08:51:49 +0600

    KS-20-2 changed lambda

--- src/main/kotlin/lesson_20/lesson20_task2.kt
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -2,16 +2,19 @@ package lesson_20
 
 const val MAX_HEALTH = 100
 
-class Player(val name: String, var health: Int)
+class Player(val name: String, var health: Int) {
+
+    fun showInfo() {
+        println("Игрок $name, здоровье: $health.")
+    }
+}
 
 fun main() {
-    val player1 = Player("Larry", 70)
-    println("Иду-иду...бац! Ух ты, волшебное зелье! Как неожиданно и приятнооо!");
+    val player1 = Player("Larry", 7)
 
-    run {
-        if (player1.health < MAX_HEALTH) player1.health = MAX_HEALTH
-        println("Будь здоров! ${player1.health}")
-    }
+    println("Мое здоровье на исходе: ${player1.health}... Ух ты, волшебное зелье! Как неожиданно и приятнооо!");
 
+    val heal = { it: Player -> if (it.health < MAX_HEALTH) it.health = MAX_HEALTH }(player1)
 
+    player1.showInfo()
 }
