92142be5328532244e6bdf0b3d9b40da5380bdb6
HM <west_ham@mail.ru>
2023-08-04 13:38:47 +0600

    KS-20-4 added lambda

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -1,5 +1,9 @@
 package lesson_20
 
-fun main() {
+const val INDEX_CORRECTION = 1
 
+fun main() {
+    val listOfElement = listOf("1-Play", "2-Pause", "3-FF", "4-Rew", "5-Rec")
+    val listOfClick = listOfElement.map {{ it: String -> "Нажат элемент $it" }(it)}
+    println (listOfClick.filter { (listOfClick.indexOf(it) + INDEX_CORRECTION) % 2 == 0 }.joinToString("\n"))
 }
