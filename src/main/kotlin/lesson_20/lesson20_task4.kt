6949a33ead50a8a93d89af8ee74627d1d75e81a8
harrymorgan <west_ham@mail.ru>
2023-08-06 00:02:04 +0600

    KS-20-4 changed filter

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -4,6 +4,6 @@ const val INDEX_CORRECTION = 1
 
 fun main() {
     val listOfElement = listOf("1-Play", "2-Pause", "3-FF", "4-Rew", "5-Rec")
-    val listOfClick = listOfElement.map {{ it: String -> "Нажат элемент $it" }(it)}
-    println (listOfClick.filter { (listOfClick.indexOf(it) + INDEX_CORRECTION) % 2 == 0 }.joinToString("\n"))
+    val listOfClick = listOfElement.map { { it: String -> "Нажат элемент $it" }(it) }
+    println(listOfClick.filterNot { (listOfClick.indexOf(it)) % 2 == 0 }.joinToString("\n"))
 }
