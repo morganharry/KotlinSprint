4941ec2831bb16df720b17fa27d253bb3ca4386f
harrymorgan <west_ham@mail.ru>
2023-08-06 00:02:23 +0600

    KS-20-4 changed filter

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -1,7 +1,5 @@
 package lesson_20
 
-const val INDEX_CORRECTION = 1
-
 fun main() {
     val listOfElement = listOf("1-Play", "2-Pause", "3-FF", "4-Rew", "5-Rec")
     val listOfClick = listOfElement.map { { it: String -> "Нажат элемент $it" }(it) }
