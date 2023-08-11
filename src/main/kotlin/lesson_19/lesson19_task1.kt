e67cd9601e015631bd1b94fa296fe1a0226eeef6
HM <west_ham@mail.ru>
2023-08-11 15:04:36 +0600

    initial

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -14,3 +14,17 @@ fun main() {
 
     for (fish in Fish.values()) println("${fish.ordinal + INDEX_CORRECTION} - ${fish.fishRus}")
 }
+/*
+все хорошо, но в enum не принято делать абстрактные функции. в этом случае можно упростить код, сделав таким образом:
+
+enum class Fish(val fishName: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок")
+} */
+/*
+Задача 1 к Уроку 19
+
+В симуляторе аквариума есть 4 вида рыб. Гуппи (guppy), скалярия (angelfish), золотая рыбка (Goldfish), петушок (Siamese fighting fish).
+Напиши enum класс, перечисляющий этих рыб. Сообщи пользователю, каких рыб он может добавить в свой аквариум, распечатав список в консоль. */
