d7b75b46322fefd23698e3e5f4ff588bca08714d
HM <west_ham@mail.ru>
2023-08-11 15:09:11 +0600

    initial

--- src/main/kotlin/lesson_20/lesson20_task2.kt
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -2,7 +2,7 @@ package lesson_20
 
 const val MAX_HEALTH = 100
 
-class Player(val name: String, var health: Int) {
+class PlayerInfo(val name: String, var health: Int) {
 
     fun showInfo() {
         println("Игрок $name, здоровье: $health.")
@@ -10,11 +10,21 @@ class Player(val name: String, var health: Int) {
 }
 
 fun main() {
-    val player1 = Player("Larry", 7)
+    val player1 = PlayerInfo("Larry", 7)
 
     println("Мое здоровье на исходе: ${player1.health}... Ух ты, волшебное зелье! Как неожиданно и приятнооо!");
 
-    val heal = { it: Player -> if (it.health < MAX_HEALTH) it.health = MAX_HEALTH }(player1)
+    val heal = { it: PlayerInfo -> if (it.health < MAX_HEALTH) it.health = MAX_HEALTH }(player1)
 
     player1.showInfo()
 }
+
+/*
+* */
+
+/*
+Задача 2 к Уроку 20
+
+В компьютерной игре игрок может подобрать лечебное зелье, которое восстанавливает всё его здоровье. Создай класс игрока
+(у него должно быть имя, текущее и максимальное здоровье).
+ Затем напиши лямбда-функцию, реализующую лечебное зелье. Создай экземпляр игрока с неполным здоровьем, примени к нему созданную лямбду-функцию.*/
